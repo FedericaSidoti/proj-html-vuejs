718af70aae2f2c1ee832791d84f421d1a6563614
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-15 23:19:47 +0100

    ADD- second array in store.js

--- src/store.js
+++ src/store.js
@@ -38,5 +38,35 @@ export const store = reactive({
             price: 110,
             src: '04-768x768.jpg'
         }
+    ],
+    deals : [
+        {
+            title: 'Shopping Mahjong Connect',
+            vote: 4, 
+            discount: 44, 
+            price: 100,
+            src: '01-768x768.jpg'
+        },
+        {
+            title: 'BangBangShooting',
+            vote: 0, 
+            discount: 0, 
+            price: 56,
+            src: '05-768x768.jpg'
+        },
+        {
+            title: 'Buddy and Lucky Solitaire',
+            vote: 0, 
+            discount: 0, 
+            price: 83,
+            src: '02-768x768.jpg'
+        },
+        {
+            title: 'Taishou x Alice Epilogue',
+            vote: 0, 
+            discount: 0, 
+            price: 90,
+            src: '03-768x768.jpg'
+        },
     ]
 })
