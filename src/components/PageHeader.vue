ab517d083f530cc9c59e31ea4eef784870c624a7
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-17 00:08:04 +0100

    ADD- on hover menu (header)

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -16,8 +16,13 @@ export default {
     <div class="container">
         <img src="/img/logo.png">
         <ul class="nav">
-            <li v-for="item in store.navItems"> 
-                <p>{{ item }}</p>
+            <li v-for="item in store.navItems" class="list-item"> 
+                <p class="nav-item">{{ item }}</p>
+                <ul class="nav-menu">
+                    <li>Left Sidebar</li>
+                    <li>Right Sidebar</li>
+                    <li>Full Width</li>
+                </ul>
             </li>
         </ul>
         <ul class="nav">
@@ -45,9 +50,36 @@ export default {
     padding: 20px 0px; 
 }
 
+.list-item {
+    position: relative; 
+
+    &:hover .nav-menu {
+        display: flex; 
+    }
+}
+
 .nav {
     display: flex;
     gap: 30px; 
+
+    .nav-item:hover{
+        color: $yellow; 
+    }
+}
+
+.nav-menu {
+    color: gray;
+    background-color: #1D1427; 
+    border: 1px solid $darkgray; 
+    position: absolute; 
+    flex-direction: column;
+    gap: 10px; 
+    font-size: 12px; 
+    padding: 10px; 
+    top: 40px; 
+    left: -10px; 
+    width: 150px;  
+    display: none;  
 }
 
 .yellow-colour{
