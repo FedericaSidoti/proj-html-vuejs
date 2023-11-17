618ca19412a597c3be2503fdb6814496ad7915a5
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-17 16:33:11 +0100

    ADD- file reset. css

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss';
+@use './styles/partials/reset.scss';
 @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;500;700&display=swap');
 
 
